9ca7f72656b70d35a3a684b634c37d1b19183379
Belat Devops <enochbelawu8@gmail.com>
2023-08-01 15:16:57 -0700

    setup gh-pages

---
+++ src/Components/Landing page/Home.js
@@ -0,0 +1,12 @@
+import styles from "../../css/Landing page/Home.module.css";
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return ( 
+        <div>
+            <Navbar />
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/Components/Landing page/Navbar.js
@@ -0,0 +1,12 @@
+import navbar from "../../css/Landing page/Navbar.module.css";
+
+
+const Navbar = () => {
+    return ( 
+        <nav className={navbar.container}>
+            <div className={navbar.logo}>African Food Incorporated</div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
